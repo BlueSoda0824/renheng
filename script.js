a66daeec9650fba064ef400e96bf0e7d3324a419
BlueSoda <161470391+BlueSoda0824@users.noreply.github.com>
2026-03-12 20:38:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,36 +13,35 @@ game.onclick = async () => {
 if(busy) return
 busy = true
 
-// 手移动到眼睛
 hand.style.transform = isMobile
 ? "translate(calc(-50% + 10px), calc(-50% - 10px))"
 : "translate(calc(-50% + 30px), calc(-50% - 35px))"
 
 await sleep(300)
 
-// 画完眼影
+
 body.src = "animal_after.png"
 
-// 加分
+
 score++
 scoreUI.textContent = "本次已为"+score+"只布菇丹画上眼影！"
 
 await sleep(400)
 
-// 手收回
+
 hand.style.transform = isMobile
 ? "translate(calc(-50% + 0px), calc(-50% + 0px))"
 : "translate(-50%, -50%)"
 
 await sleep(300)
 
-// 淡出
+
 body.style.opacity = 0
 hand.style.opacity = 1
 
 await sleep(1000)
 
-// 重置
+
 body.src = "animal_before.png"
 body.style.opacity = 1
 hand.style.opacity = 1
