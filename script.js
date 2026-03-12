4acc21cf76e826ba6942e4812fecef0d746457fa
BlueSoda <161470391+BlueSoda0824@users.noreply.github.com>
2026-03-12 16:54:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-let score = 0
-let busy = false
-
-const animal = document.getElementById("animal")
-const scoreUI = document.getElementById("score")
-
-animal.onclick = async () => {
-
-if(busy) return
-busy = true
-
-// 1 伸手
-animal.src = "animal_hand.png"
-
-await sleep(300)
-
-// 2 画完
-animal.src = "animal_after.png"
-
-// 3 加分
-score++
-scoreUI.textContent = score
-
-await sleep(500)
-
-// 4 慢慢消失
-animal.style.opacity = 0
-
-await sleep(1000)
-
-// 5 重置
-animal.src = "animal_before.png"
-animal.style.opacity = 1
-
-busy = false
-
+body{
+margin:0;
+height:100vh;
+display:flex;
+justify-content:center;
+align-items:center;
+background:#f5f5f5;
+font-family:sans-serif;
 }
 
-function sleep(ms){
-return new Promise(r=>setTimeout(r,ms))
+#score{
+position:fixed;
+top:20px;
+right:30px;
+font-size:30px;
+font-weight:bold;
+}
+
+.character{
+position:relative;
+width:300px;
+height:300px;
+}
+
+#body{
+width:100%;
+position:absolute;
+left:0;
+top:0;
+}
+
+#hand{
+width:100%;
+position:absolute;
+left:0;
+top:0;
+
+transform: translate(0px,0px);
+transition: transform 0.3s ease;
 }
